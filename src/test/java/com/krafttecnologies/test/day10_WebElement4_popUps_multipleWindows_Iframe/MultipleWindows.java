4775b7c9e36a9095c0397608cf32e90641e6ed7b
HP <ihsanakcair@gmail.com>
2022-11-15 18:33:01 +0300

    Web Elements

---
+++ src/test/java/com/krafttecnologies/test/day10_WebElement4_popUps_multipleWindows_Iframe/MultipleWindows.java
@@ -0,0 +1,86 @@
+package com.krafttecnologies.test.day10_WebElement4_popUps_multipleWindows_Iframe;
+
+import com.krafttecnologies.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
+
+public class MultipleWindows {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+//        driver.close();  //current windowu kapatır...
+        driver.quit(); //açılan tüm penceleri kapatır...
+    }
+
+    @Test
+    public void twoWindows() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/windows");
+
+        String titleBeforeClick =driver.getTitle();
+        System.out.println("titleBeforeClick = " + titleBeforeClick);
+
+        WebElement click_here = driver.findElement(By.linkText("Click Here"));
+        click_here.click();
+        Thread.sleep(2000);
+
+//        String titleAfterClick= driver.getTitle();
+//        System.out.println("titleAfterClick = " + titleAfterClick);
+
+        String currentTab = driver.getWindowHandle(); //title The Internet and has ID=506B1B9F7D87D7355C3F5AA44BA4B365
+        System.out.println("currentTab = " + currentTab);
+
+       Set<String> windowHandles = driver.getWindowHandles();
+
+        for (String tab : windowHandles) {// go and check one by one
+            if (!tab.equals(currentTab)) {
+                driver.switchTo().window(tab);
+            }
+        }
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+    }
+
+    @Test
+    public void multipleWindows() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/windows");
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+        System.out.println("driver.getWindowHandle() = " + driver.getWindowHandle());
+
+        WebElement click_here = driver.findElement(By.linkText("Click Here"));
+        click_here.click();
+        click_here.click();
+        click_here.click();
+        click_here.click();
+        click_here.click();
+        Thread.sleep(5000);
+
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        for (String tab : windowHandles) {
+            driver.switchTo().window(tab);
+            if (driver.getTitle().equals("New Window"))
+//            if (driver.getCurrentUrl().equals("https://the-internet.herokuapp.com/windows/new"))
+            {
+                //stop loop
+                break;
+            }
+
+        }
+        System.out.println("after click driver.getTitle() = " + driver.getTitle());
+    }
+}
+
---
+++ src/test/java/com/krafttecnologies/test/day9_WebElements3/DropDownWithoutSelect.java
@@ -0,0 +1,66 @@
+package com.krafttecnologies.test.day9_WebElements3;
+
+import com.krafttecnologies.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DropDownWithoutSelect {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        //Dropdown menuyu locate ediyoruz...
+        driver.get("https://demoqa.com/select-menu");
+        WebElement dropDownMenu=driver.findElement(By.xpath("( //div[@class=' css-1wa3eu0-placeholder'])[1]"));
+        System.out.println("dropDownMenu.getText() = " + dropDownMenu.getText());
+
+        //kutudaki ilk elementi assert edelim....
+
+        String expectedOption="Select Option";
+        String actualOption=dropDownMenu.getText();
+
+        Assert.assertEquals(actualOption,expectedOption,"FAIL");
+
+        //Dropdown içerisinde Group 2 Option 1 seceneğini çağıralım...
+        //sorun inspect yapılmak istendiğinde kaybolmasıdır.
+        //aşmak için Event Listeners dan blur altındaki öğeler remove edilir...
+
+        dropDownMenu.click();
+
+        WebElement group2Option1= driver.findElement(By.cssSelector("#react-select-2-option-1-0"));
+
+        group2Option1.click();
+
+//        String expected="Group 2, option 1";  //buradan verify olmuyor..onun yerine tıklandıktan sonraki halini yeniden
+//        String actual=group2Option1.getText(); //locate edip getText yapmalıyız.... aşağıdaki gibi...
+//        System.out.println("actual = " + actual);
+//        Assert.assertEquals(actual,expected,"FAIL");
+
+        //Group 2 Option 1 seçildikten sonra verify etmek için tekrar locate ediyoruz...
+
+        WebElement selectOption=driver.findElement(By.xpath("//div[@class=' css-1uccc91-singleValue']"));
+        System.out.println("selectOption.getText() = " + selectOption.getText());
+
+        String expectedOption2="Group 2, option 1";
+        String actualOption2=selectOption.getText();
+
+        Assert.assertEquals(actualOption2,expectedOption2,"FAIL");
+
+        Thread.sleep(2000);
+    }
+}
